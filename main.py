176a890ecc3e47911be46c2eda4aba195c7cdc9a
Jose Carrillo <josealejandro2503@gmail.com>
2024-01-25 12:42:28 -0500

    Tu nuevo mensaje de commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+elements = "+-/*!&$#?=@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+lon = int( input("Escribe la longitud:") )
+
+password = ""
+
+for i in range(lon):
+    password += random.choice(elements)
+
+print(password)
+
